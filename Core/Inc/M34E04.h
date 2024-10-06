463832aa64a9d68ba08f9cfbb00fbc5e09389458
Mickey <mickey_fraser@outlook.com>
2024-10-05 21:18:43 -0500

    Added EEPROM files

---
+++ Core/Inc/M34E04.h
@@ -0,0 +1,16 @@
+/*
+ * M34E04.h
+ *
+ *  Created on: Oct 5, 2024
+ */
+
+#ifndef INC_M34E04_H_
+#define INC_M34E04_H_
+
+#define M32E04_ADDR (0b1010000 << 1)
+#define M32E04_SET_PAGE0 (0b0110110 << 1)
+#define M32E04_SET_PAGE1 (0b0110111 << 1)
+#define M32E04_READ_PAGE (0b0110110 << 1)
+
+
+#endif /* INC_M34E04_H_ */
