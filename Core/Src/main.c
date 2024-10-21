6ad5290793e4fab5da22e20a544a4255e8b4d9c4
Mickey <mickey_fraser@outlook.com>
2024-10-21 17:05:57 -0500

    Added lsm6sv driver

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
+#include "lsm6dsv_reg.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
